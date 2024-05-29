5afef0e7df47fbe54e63a6c60043a2deaf220457
LeeYeJunZero <165781116+LeeYeJunZero@users.noreply.github.com>
2024-05-29 13:18:38 +0900

    Create MyCharacter.h

---
+++ AIController/MyCharacter.h
@@ -0,0 +1,15 @@
+// MyCharacter.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyCharacter.generated.h"
+
+UCLASS()
+class YOURPROJECTNAME_API AMyCharacter : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    AMyCharacter();
+};
